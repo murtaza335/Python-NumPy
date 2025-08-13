e0545b22ce1d3a1bb3107ce60fb548baa4922be5
murtaza335 <mmurtaza.bscs24seecs@seecs.edu.pk>
2025-08-13 16:40:57 +0500

    vectorized operations

---
+++ 04_vectorized_operations.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b)   # [5 7 9]
+print(a - b)   # [-3 -3 -3]
+print(a * b)   # [ 4 10 18]
+print(a / b)   # [0.25 0.4  0.5 ]
